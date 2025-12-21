c7178030b1969726a21b7688278bac59f9331011
Ayush prajapati <ayushprajapati15806@gmail.com>
2025-12-21 14:42:41 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://trendies-frontend-b7wv.onrender.com", "http://localhost:5174"],
     credentials:true
 }))
 
