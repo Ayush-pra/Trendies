50b19941715a1b72a2f771ed8ef2ae99dd17d5eb
Ayush prajapati <ayushprajapati15806@gmail.com>
2025-12-21 14:48:47 +0530

    Update Navbar.jsx

--- Admin/src/components/Navbar.jsx
+++ Admin/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { authDataContext } from '../../context/authContext';
+import { authDataContext } from '../../context/AuthContext';
 import { adminDataContext } from '../../context/AdminContext';
 import { toast } from 'react-toastify';
 import Loading from './Loading';
