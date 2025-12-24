6a16dd306750e32301b82e46569e5bd2b2f59224
Ayush prajapati <ayushprajapati15806@gmail.com>
2025-12-24 20:09:11 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react';
 export const authDataContext = createContext();
 
 const AuthContext = ({children}) => {
-    const serverUrl = "https://trendies-backend-19xy.onrender.com"
+    const serverUrl = "https://trendies-backend-19xy.onrender.com" 
     const value = {
         serverUrl
     }
