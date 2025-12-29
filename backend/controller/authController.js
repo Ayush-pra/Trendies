302cbddae9aeaa57bea1b8ae8c81e999fd3e5b41
Ayush prajapati <ayushprajapati15806@gmail.com>
2025-12-29 20:45:33 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,128 +1,16 @@
-// const { genToken, genTokenAdmin } = require("../config/token.js");
-// const User = require("../model/userModel.js");
-// const validator = require("validator");
-// const bcrypt = require("bcryptjs");
-// const register = async (req, res) => {
-//     try {
-//         const { name, email, password } = req.body;
-//         const existUser = await User.findOne({ email })
-//         if (existUser) {
-//             return res.status(400).json({ message: "User Already Exist. Please Sign in" })
-//         }
-//         if (!validator.isEmail(email)) {
-//             return res.status(400).json({ message: "Enter Valid Email" })
-//         }
-//         if (password.length < 6) {
-//             return res.status(400).json({ message: "Enter Atleast 6 characters" })
-//         }
-//         const hashPassword = await bcrypt.hash(password, 10);
-
-//         const user = await User.create({
-//             name,
-//             email,
-//             password: hashPassword
-//         })
-//         const token = await genToken(user._id);
-//         res.cookie("token", token);
-//         return res.status(201).json(user)
-//     }
-//     catch (error) {
-//         console.log("Sign up error");
-//         res.status(500).json({ message: `Sign up error : ${error}` });
-//     }
-// }
-
-// const login = async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
-//         const user = await User.findOne({ email });
-//         if (!user) {
-//             return res.status(404).json({ message: "Incorrect email or password" });
-//         }
-//         const isMatch = await bcrypt.compare(password, user.password);
-//         if (!isMatch) {
-//             return res.status(404).json({ message: "Incorrect email or password" });
-//         }
-//         const token = await genToken(user._id);
-//         res.cookie("token", token);
-//         return res.status(201).json(user)
-//     }
-//     catch (error) {
-//         console.log("Sign in error");
-//         res.status(500).json({ message: `Sign in error : ${error}` });
-//     }
-// }
-
-// const logout = (req, res) => {
-//     try {
-//         res.clearCookie("token");
-//         return res.status(200).json({ message: "Visit Again" })
-//     }
-//     catch (error) {
-//         console.log("Sign out error");
-//         res.status(500).json({ message: `Sign out error : ${error}` });
-//     }
-// }
-
-// const googleLogin = async (req, res) => {
-//     try {
-//         const { name, email } = req.body;
-//         const user = await User.findOne({ email });
-//         if (!user) {
-//             user = await User.create({ name, email })
-//         }
-//         const token = await genToken(user._id);
-//         res.cookie("token", token);
-//         return res.status(201).json(user)
-//     }
-//     catch (error) {
-//         console.log("Google Sign in error");
-//         res.status(500).json({ message: `Google Sign in error : ${error}` });
-//     }
-
-// }
-
-// const adminLogin = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-//       const token = await genTokenAdmin(email);
-
-//       res.cookie("token", token, {
-//         httpOnly: true,
-//         secure: false,        // true in production (HTTPS)
-//         sameSite: "lax",   
-//       });
-
-//       return res.status(201).json({ message: "Login successful" });
-//     }
-
-//     return res.status(401).json({ message: "Invalid credentials" });
-//   } catch (error) {
-//     console.log("Admin Sign in error", error);
-//     res.status(500).json({ message: `Admin Sign in error : ${error}` });
-//   }
-// };
-
-
-// module.exports = { register, login, logout, googleLogin, adminLogin };
-
 const { genToken, genTokenAdmin } = require("../config/token.js");
 const User = require("../model/userModel.js");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
-/* 🔐 Cookie configuration (FIXED) */
 const isProd = process.env.NODE_ENV === "production";
 
 const cookieOptions = {
   httpOnly: true,
-  secure: isProd,                 // true on Render
+  secure: isProd,          
   sameSite: isProd ? "None" : "Lax",
 };
 
-/* ================= REGISTER ================= */
 const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -158,7 +46,6 @@ const register = async (req, res) => {
   }
 };
 
-/* ================= LOGIN ================= */
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -183,7 +70,6 @@ const login = async (req, res) => {
   }
 };
 
-/* ================= LOGOUT ================= */
 const logout = async (req, res) => {
   try {
     res.clearCookie("token", cookieOptions);
@@ -194,7 +80,6 @@ const logout = async (req, res) => {
   }
 };
 
-/* ================= GOOGLE LOGIN ================= */
 const googleLogin = async (req, res) => {
   try {
     let { name, email } = req.body;
@@ -214,7 +99,6 @@ const googleLogin = async (req, res) => {
   }
 };
 
-/* ================= ADMIN LOGIN ================= */
 const adminLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
