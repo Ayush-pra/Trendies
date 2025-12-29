2d421c07b5d17381af45a45e7b3160119afb276c
Ayush prajapati <ayushprajapati15806@gmail.com>
2025-12-29 20:30:47 +0530

    Update Sidebar.jsx

--- Admin/src/components/Sidebar.jsx
+++ Admin/src/components/Sidebar.jsx
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import { IoBagAdd } from "react-icons/io5";
-// import { FaListCheck } from "react-icons/fa6";
-// import { MdBorderColor } from "react-icons/md";
-// import { useNavigate } from 'react-router-dom';
-// const Sidebar = () => {
-//   const navigate = useNavigate();
-//   return (
-//     <div className='w-[18%] min-h-[100vh] border border-r-[1px] py-[60px] fixed left-0 top-0'>
-//       <div className='flex flex-col gap-4 pt-[40px] pl-[20%] text-[15px]'>
-//         <div className='flex items-center justify-center md:justify-start gap-3 border border-gray-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-amber-800' onClick={()=>{navigate("/add")}}>
-//           <IoBagAdd className='w-[20px] h-[20px]'/>
-//           <p>Add items </p>
-//         </div>
-//         <div className='flex items-center justify-center md:justify-start gap-3 border border-gray-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-amber-800' onClick={()=>{navigate("/list")}}>
-//           <FaListCheck className='w-[20px] h-[20px]'/>
-//           <p>List items </p>
-//         </div>
-//         <div className='flex items-center justify-center md:justify-start gap-3 border border-gray-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-amber-800' onClick={()=>{navigate("/orders")}}>
-//           <MdBorderColor className='w-[20px] h-[20px]'/>
-//           <p>View Orders </p>
-//         </div>
-        
-//       </div>
-//     </div> 
-//   );
-// }
-
-// export default Sidebar;
-
-
 import React from "react";
 import { IoBagAdd } from "react-icons/io5";
 import { FaListCheck } from "react-icons/fa6";
