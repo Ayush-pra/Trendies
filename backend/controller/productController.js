fc25f1c79ede900dc855629fc0b6ace09bd2f182
Ayush prajapati <ayushprajapati15806@gmail.com>
2025-12-29 20:47:49 +0530

    Update productController.js

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,123 +1,6 @@
-// const uploadOnCloudinary = require("../config/cloudinary.js");
-// const Product = require("../model/productModel.js");
-
-// const addProduct = async(req, res)=>{
-//     try{
-//         const {name, description, price, category, subCategory, sizes, bestseller} = req.body;
-//         const image1 = await uploadOnCloudinary(req.files.image1[0].path);
-//         const image2 = await uploadOnCloudinary(req.files.image2[0].path);
-//         const image3 = await uploadOnCloudinary(req.files.image3[0].path);
-//         const image4 = await uploadOnCloudinary(req.files.image4[0].path);
-
-//         const productData = {
-//             name, 
-//             description, 
-//             price : Number(price), 
-//             category, 
-//             subCategory, 
-//             sizes : JSON.parse(sizes), 
-//             bestseller : bestseller==="true" ? true : false,
-//             date : Date.now(),
-//             image1,
-//             image2,
-//             image3,
-//             image4
-//         }
-
-//         const product = await Product.create(productData);
-
-//         return res.status(201).json(product);
-//     }
-//     catch{
-//         console.log("productData error");
-//     }
-// }
-
-// const listproduct = async (req, res) => {
-//     try{
-//         const product = await Product.find({});
-//         return res.status(200).json(product);
-//     }
-//     catch(error){
-//         console.log(error)
-//     }
-// }
-
-// const removeproduct = async (req, res) => {
-//     try{
-//         const {id} = req.params;
-//         const product = await Product.findByIdAndDelete(id);
-//         return res.status(200).json(product);
-//     }
-//     catch(error){
-//         console.log(error)
-//     }
-// }
-
-// module.exports = {addProduct, removeproduct, listproduct};
-
 const uploadOnCloudinary = require("../config/cloudinary.js");
 const Product = require("../model/productModel.js");
 
-/* =========================
-   ADD PRODUCT
-========================= */
-// const addProduct = async (req, res) => {
-//   try {
-//     const {
-//       name,
-//       description,
-//       price,
-//       category,
-//       subCategory,
-//       sizes,
-//       bestseller,
-//     } = req.body;
-
-//     // 🔒 Basic validation
-//     if (!name || !price || !category || !sizes) {
-//       return res.status(400).json({ message: "Missing required fields" });
-//     }
-
-//     // 🔒 At least one image required
-//     if (!req.files || !req.files.image1) {
-//       return res.status(400).json({ message: "At least one image is required" });
-//     }
-
-//     // 🔁 Safe image uploader
-//     const uploadImage = async (img) => {
-//       if (!img || !img[0]) return null;
-//       return await uploadOnCloudinary(img[0].path);
-//     };
-
-//     const image1 = await uploadImage(req.files.image1);
-//     const image2 = await uploadImage(req.files.image2);
-//     const image3 = await uploadImage(req.files.image3);
-//     const image4 = await uploadImage(req.files.image4);
-
-//     const productData = {
-//       name,
-//       description,
-//       price: Number(price),
-//       category,
-//       subCategory,
-//       sizes: JSON.parse(sizes),
-//       bestseller: bestseller === "true",
-//       date: Date.now(),
-//       image1,
-//       image2,
-//       image3,
-//       image4,
-//     };
-
-//     const product = await Product.create(productData);
-
-//     return res.status(201).json(product);
-//   } catch (error) {
-//     console.log("productData error:", error);
-//     return res.status(500).json({ message: "Product upload failed" });
-//   }
-// };
 const addProduct = async (req, res) => {
   try {
     const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
@@ -163,9 +46,6 @@ const addProduct = async (req, res) => {
   }
 };
 
-/* =========================
-   LIST PRODUCTS
-========================= */
 const listproduct = async (req, res) => {
   try {
     const products = await Product.find({});
@@ -176,9 +56,6 @@ const listproduct = async (req, res) => {
   }
 };
 
-/* =========================
-   REMOVE PRODUCT
-========================= */
 const removeproduct = async (req, res) => {
   try {
     const { id } = req.params;
