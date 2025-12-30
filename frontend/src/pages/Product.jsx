f58809117b0e8727901f672d48ad00961495d00a
Ayush prajapati <ayushprajapati15806@gmail.com>
2025-12-30 20:03:54 +0530

    Update Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -4,31 +4,10 @@ import BestSeller from '../components/BestSeller';
 
 const Product = () => {
   return (
-    // <div className='w-full bg-gradient-to-l from-[#141414] to-[#0c2025] min-h-screen flex flex-col items-center justify-start py-10'>
-    //   {/* Latest Collection */}
-    //   <div className='w-full flex items-center justify-center'>
-    //     <LatestCollection/>
-    //   </div>
-
-    //   {/* Best Seller */}
-    //   <div className='w-full flex items-center justify-center mt-10'>
-    //     <BestSeller/>
-    //   </div>
-    // </div>
     <div className="w-full bg-gradient-to-l from-[#040404] to-[#050c0e] py-14">
       <LatestCollection />
       <BestSeller />
     </div>
   )
 }
-
-// const Product = () => {
-//     return (
-//         <div className='w-full bg-slate-950 min-h-screen flex flex-col items-center justify-start py-10'>
-//             <LatestCollection/>
-//             <BestSeller/>
-//         </div>
-//     );
-// };
-
 export default Product;
