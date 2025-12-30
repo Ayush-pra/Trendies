b9763583929242f6b5a150cb5ff25bdd6085ecd8
Ayush prajapati <ayushprajapati15806@gmail.com>
2025-12-30 19:51:44 +0530

    Update BestSeller.jsx

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -17,13 +17,6 @@ const BestSeller = () => {
         <Title text1={'BEST'} text2={'SELLER'}></Title>
         <p className='w-[100%] m-auto text-[20px] px-[10px] text-blue-100'>Tried, Tested, Loved Discover Our All-Time BestSeller</p>
       </div>
-      {/* <div className='w-[100%] h-[50%] mt-[30px] flex items-center justify-center flex-wrap gap-[50px]'>
-        {
-          bestSeller.map((item,index)=>(
-            <Card1 key={index} name={item.name} id={item._id} price={item.price} image={item.image1}/>
-          ))
-        }
-      </div> */}
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4">
         {
           bestSeller.map((item,index)=>(
